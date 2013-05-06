4da8b488d9862bb5ca076c78b5a070977cc31242
Dean Perry <dean@deanperry.net>
2013-05-06 13:23:35 +0100

    added passport

---
+++ lib/capistrano-voupe/passport.rb
@@ -0,0 +1,14 @@
+Capistrano::Configuration.instance.load do
+
+	namespace :symlink do
+		desc <<-DESC
+		Symlinks shared/config/passport.key to current/config/passport.key
+		DESC
+		task :passport, :roles => :app, :except => {:no_release => true} do
+			run "ln -nfs #{shared_path}/config/passport.key #{release_path}/config/passport.key"
+		end
+	end
+
+	after "mysql:symlink", "symlink:passport"
+
+end
